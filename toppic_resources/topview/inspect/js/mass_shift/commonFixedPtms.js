99b4fe1d4122e10a5a6864632fca10206b4303f2
inkwon <inkwchoi@iu.edu>
2020-05-21 14:39:21 -0400

    fixed #4 - replaced dynamic download js files

--- toppic_resources/topview/inspect/js/mass_shift/commonFixedPtms.js
+++ toppic_resources/topview/inspect/js/mass_shift/commonFixedPtms.js
@@ -78,7 +78,7 @@ class commonFixedPtms {
 		removeButton.setAttribute("class","form-control btn btn-default btn-sm removerow");
 		
 		let iRemoveFrame = document.createElement("i");
-		iRemoveFrame.setAttribute("class","fa fa-remove");
+		iRemoveFrame.setAttribute("class","fa fa-times");
 		removeButton.appendChild(iRemoveFrame);
 		
 		fixedptmsdiv.appendChild(inputAcid);
